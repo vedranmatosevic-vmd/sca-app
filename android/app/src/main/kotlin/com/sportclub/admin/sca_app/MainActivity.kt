b26cb55ebb13d7e099da982a7397986fa40295ab
vedranmatosevic-sc <vedran.matosevic@smartcode.eu>
2022-06-01 22:22:40 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/sportclub/admin/sca_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sportclub.admin.sca_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+        title: const Text("data"),
+        ),
+        body: const Center(
+          child: RandomWords(),
+        ),
+      )
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+
